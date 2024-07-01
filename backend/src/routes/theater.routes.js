6f1dcebb1806ab9e20b60ee65a9b9ee982a6397f
Majid Ali <lahmajidali@gmail.com>
2024-07-01 13:49:00 +0500

    updated structure

--- backend/src/routes/theater.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { validate } from '../validators/validate.validator.js';
-import { mongodbPathIdValidator } from '../validators/mongodb.validator.js';
-import { verifyJWT } from '../middlewares/verifyJWT.middleware.js';
-
-const router = express.Router();
-
-export default router;
