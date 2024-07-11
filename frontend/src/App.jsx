3d3e54e7b4a13ed9fbe2e311404b3495e50b2fa9
Majid Ali <lahmajidali@gmail.com>
2024-07-11 15:55:26 +0500

    deleted all components to re-build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import AppLayout from "./components/AppLayout";
-import Feeds from "./components/Feeds";
-import Reels from "./components/Reels";
-import Search from "./components/Search";
-import Explore from "./components/Explore";
-import Profile from "./components/Profile";
-import Chats from "./components/Chats";
+import { Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Feeds />} />
-          <Route path="reels" element={<Reels />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="search-something" element={<Search />} />
-          <Route path="direct/inbox" element={<Chats />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </>
+      <Routes></Routes>
   );
 }
--- frontend/src/components/Chats.jsx
+++
@@ -1,5 +0,0 @@
-const Chats = () => {
-  return <div>Chats</div>;
-};
-
-export default Chats;
